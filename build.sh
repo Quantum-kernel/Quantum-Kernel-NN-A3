d9764f803a759c729efea6ca4dc7b8cf25136bc7
prashantpaddune <elonpras@my.smccd.edu>
2018-05-21 15:14:54 +0530

    round two

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+DTS=arch/arm64/boot/dts
+RDIR=$(pwd)
+# GCC
+export CROSS_COMPILE=/home/prashantp/UBERTC-aarch64-linux-android-4.9/bin/aarch64-linux-android-
+# Cleanup
+make clean && make mrproper
+# J700F NN Defcon
+make a3xelte_00_defconfig
+make exynos7580-a3xelte_eur_open_00.dtb exynos7580-a3xelte_eur_open_01.dtb exynos7580-a3xelte_eur_open_02.dtb
+# Make zImage
+make ARCH=arm64 -j5
+# Make DT.img
+./tools/dtbtool -o ./boot.img-dtb -v -s 2048 -p ./scripts/dtc/ $DTS/
+# Cleaup
+rm -rf $DTS/.*.tmp
+rm -rf $DTS/.*.cmd
+rm -rf $DTS/*.dtb
